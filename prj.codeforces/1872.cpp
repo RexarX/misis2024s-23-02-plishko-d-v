84e999f7e5f120475aa861442354d873c12cf9aa
Denis <96687186+RexarX@users.noreply.github.com>
2023-09-15 19:39:46 +0300

    Add files via upload

---
+++ prj.codeforces/1872.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+ 
+int main()
+{
+	int t, a, b, c;
+	std::cin >> t;
+	for (int i = 0; i < t; i++) {
+		std::cin >> a >> b >> c;
+		std::cout << (std::max(a, b) - (a + b) / 2 + c - 1) / c << std::endl;
+	}
+ 
+	return 0;
+}
