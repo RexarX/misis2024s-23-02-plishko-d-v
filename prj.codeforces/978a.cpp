785aca9fd3b858e324afe667d1c572e3df000e2f
Denis <96687186+RexarX@users.noreply.github.com>
2023-09-15 18:54:15 +0300

    Add files via upload

---
+++ prj.codeforces/978a.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void duplicates(std::vector<int>& mas, int element) {
+	auto it = std::find(mas.begin(), mas.end(), element);
+	if (it != mas.end())
+	{
+		mas.erase(mas.begin() + (it - mas.begin()));
+	}
+}
+
+int main()
+{
+	int n;
+	std::cin >> n;
+	std::vector<int> mas;
+	for (int i = 0; i < n; i++) {
+		int element;
+		std::cin >> element;
+		duplicates(mas, element);
+		mas.push_back(element);
+	}
+
+	std::cout << mas.size() << std::endl;
+	for (int i = 0; i < mas.size(); i++) {
+		std::cout << mas[i] << " ";
+	}
+
+	return 0;
+}
