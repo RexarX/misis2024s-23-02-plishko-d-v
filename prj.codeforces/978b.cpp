4b329c2db7804bf9fbb801cbcf9f62227e47380a
Denis <96687186+RexarX@users.noreply.github.com>
2023-09-15 18:00:52 +0300

    Add files via upload

---
+++ prj.codeforces/978b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	int n, cnt(0);
+	std::string word;
+	std::cin >> n;
+	std::cin >> word;
+	for (int i = 0; i < n - 2; i++) {
+		if (word.substr(i, 3) == "xxx") {
+			cnt++;
+		}
+	}
+
+	std::cout << cnt;
+	return 0;
+}
