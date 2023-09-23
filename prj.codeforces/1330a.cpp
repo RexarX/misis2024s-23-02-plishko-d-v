f5feee1d5d36f6de6ef79597c59732592f8b9fab
Denis <96687186+RexarX@users.noreply.github.com>
2023-09-23 11:28:56 +0300

    Add files via upload

---
+++ prj.codeforces/1330a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+ 
+ 
+int main()
+{
+	int x, n, t, element;
+	std::cin >> t;
+	for (int i = 0; i < t; i++) {
+		std::cin >> n >> x;
+		std::vector<bool> check(201);
+		for (int j = 0; j < n; j++) {
+			std::cin >> element;
+			element--;
+			check[element] = true;
+		}
+ 
+		int k(0), cnt(0);
+		while (cnt < x || check[k]) {
+			cnt += !check[k];
+			k++;
+		}
+ 
+		std::cout << k << std::endl;
+	}
+}
