e503b51978e8b324c9cfd67914c810c55e2bc509
Denis <96687186+RexarX@users.noreply.github.com>
2023-09-23 12:31:19 +0300

    Add files via upload

---
+++ prj.codeforces/1721b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main()
+{
+	int t, n, m, sx, sy, d;
+	std::cin >> t;
+	for (int i = 0; i < t; i++) {
+		std::cin >> n >> m >> sx >> sy >> d;
+		if (((sx + d >= n) && (sx - d <= 1)) || ((sy + d >= m) && (sy - d <= 1)) || 
+				((sx - d <= 1) && (sy - d <= 1)) || ((sx + d >= n) && (sy + d >= m))) {
+				std::cout << "-1" << std::endl;
+		}
+		else {
+				std::cout << (n - 1) + (m - 1) << std::endl;
+		}
+	}
+}
