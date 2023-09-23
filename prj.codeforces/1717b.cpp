5d422e723bb65692493210f4b596e938f3252e95
Denis <96687186+RexarX@users.noreply.github.com>
2023-09-23 19:14:13 +0300

    Add files via upload

---
+++ prj.codeforces/1717b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+int main()
+{
+	int t, n, k, r, c;
+	std::cin >> t;
+	while (t--) {
+		std::cin >> n >> k >> r >> c;
+		for (int i = 1;i <= n;std::cout << std::endl, i++) {
+			for (int j = 1;j <= n;j++) {
+				if ((i + j) % k == (r + c) % k) {
+					std::cout << 'X';
+				}
+				else {
+					std::cout << '.';
+				}
+			}
+		}
+	}
+
+	return 0;
+}
