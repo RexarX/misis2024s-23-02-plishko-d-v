966ac50d3cad3ad977db8ce2545a480fc09d0cc4
Denis <96687186+RexarX@users.noreply.github.com>
2023-09-30 14:37:42 +0300

    Add files via upload

---
+++ prj.codeforces/1670b.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+ 
+ 
+int alg(std::string& s, const std::vector<char>& symbols, int n) {
+	int res(0), last(0);
+	for (int i = 0; i < n; i++) {
+		if (std::find(symbols.begin(), symbols.end(), s[i]) != symbols.end()) {
+			if (i - last > res) {
+				res = i - last;
+			}
+
+			last = i;
+		}
+	}
+ 
+	return res;
+}
+ 
+int main() {
+	std::ios::sync_with_stdio(false);
+	int t, n, k;
+	std::string s;
+	std::vector<char> symbols;
+	char symbol;
+ 
+	std::cin >> t;
+	for (int i = 0; i < t; i++) {
+		symbols.clear();
+		std::cin >> n;
+		std::cin >> s;
+		std::cin >> k;
+		for (int j = 0; j < k; j++) {
+			std::cin >> symbol;
+			symbols.push_back(symbol);
+		}
+ 
+		std::cout << alg(s, symbols, n) << '\n';
+	}
+ 
+	return 0;
+}
