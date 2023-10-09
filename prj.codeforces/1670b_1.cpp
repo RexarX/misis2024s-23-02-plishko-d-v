482b503a565d3201b5f5cb94c31564c0b6b63190
Denis <96687186+RexarX@users.noreply.github.com>
2023-10-09 21:19:50 +0300

    Update 1670b_1.cpp

--- prj.codeforces/1670b_1.cpp
+++ prj.codeforces/1670b_1.cpp
@@ -2,11 +2,11 @@
 #include <string>
  
  
-bool contains(const std::string& str, char& targetChar) {
+bool contains(const std::string& str, const char& targetChar) {
 	return str.find(targetChar) != std::string::npos;
 }
  
-int alg(std::string& s, const std::string& symbols, int len, int cnt) {
+int alg(std::string& s, const std::string& symbols, const int& len, int cnt) {
 	int i(0);
 	while (i != s.length() - 1) {
 		if (contains(symbols, s[i + 1])) {
