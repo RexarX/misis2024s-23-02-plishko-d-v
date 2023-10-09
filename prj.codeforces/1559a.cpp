575e7ef9befb4ee50d14d9984ceecd8581d7c1d9
Denis <96687186+RexarX@users.noreply.github.com>
2023-10-09 20:32:19 +0300

    Add files via upload

---
+++ prj.codeforces/1559a.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+
+int alg(const std::vector<int>& mas, const int& n){
+  int res = mas[0];
+  for (int i = 1; i < n; i++) {
+    res &= mas[i];
+  }
+
+  return res;
+}
+
+int main()
+{
+  int t, n, element;
+  std::vector<int> mas;
+  std::cin >> t;
+  for (int i = 0; i < t; i++) {
+    mas.clear();
+    std::cin >> n;
+    for (int j = 0; j < n; j++) {
+      std::cin >> element;
+      mas.push_back(element);
+    }
+
+    std::cout << alg(mas, n) << '\n';
+  }
+
+  return 0;
+}
