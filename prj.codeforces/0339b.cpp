077c6b2cf6a62146694a85bcb04443b6ee8855ec
Denis <96687186+RexarX@users.noreply.github.com>
2023-10-14 14:53:24 +0300

    Add files via upload

---
+++ prj.codeforces/0339b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main()
+{
+	long long int n, m, a, previous(1), result(0);
+	std::cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		std::cin >> a;
+		result += (n + a - previous) % n;
+		previous = a;
+	}
+
+	std::cout << result;
+	return 0;
+}
