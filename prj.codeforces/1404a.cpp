0945139a9475a86a9101bf81bb6e51e97e3e6335
Denis <96687186+RexarX@users.noreply.github.com>
2023-10-14 14:18:07 +0300

    Add files via upload

---
+++ prj.codeforces/1404a.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+
+bool isBalanced(std::string& s, const int& n, const int& k) {
+	for (int i = 0; i < n; i++) {
+		if (s[i % k] == '?') {
+			s[i % k] = s[i];
+		}
+
+		if (s[i] != s[i % k] && s[i] != '?') {
+			return false;
+		}
+	}
+
+	if (std::count(s.begin(), s.end() - n + k, '0') <= k / 2 && std::count(s.begin(), s.end() - n + k, '1') <= k / 2) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int main() 
+{
+	int t, n, k;
+	std::string s;
+	std::cin >> t;
+	for (int i = 0; i < t; i++) {
+		std::cin >> n >> k;
+		std::cin >> s;
+		if (isBalanced(s, n, k)) {
+			std::cout << "YES" << '\n';
+		}
+		else {
+			std::cout << "NO" << '\n';
+		}
+	}
+
+	return 0;
+}
