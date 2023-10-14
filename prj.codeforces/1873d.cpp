7fb071ff210168606eaec129c4bcca490fbc9e06
Denis <96687186+RexarX@users.noreply.github.com>
2023-10-14 15:15:49 +0300

    Add files via upload

---
+++ prj.codeforces/1873d.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+int main()
+{
+	int t, n, k, cnt, mx;
+	char element;
+	std::cin >> t;
+	for (int i = 0; i < t; i++) {
+		std::cin >> n >> k;
+		cnt = 0;
+		mx = -1;
+		for (int j = 0; j < n; j++) {
+			std::cin >> element;
+			if (element == 'B' && j > mx) {
+				mx = j + k - 1;
+				cnt++;
+			}
+		}
+
+		std::cout << cnt << '\n';
+	}
+
+	return 0;
+}
