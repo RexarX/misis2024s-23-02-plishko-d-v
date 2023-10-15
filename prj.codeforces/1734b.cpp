78db074e05228b579149ae165555510f82408344
Denis <96687186+RexarX@users.noreply.github.com>
2023-10-15 17:10:58 +0300

    Add files via upload

---
+++ prj.codeforces/1734b.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+int main()
+{
+	int t, n;
+	std::cin >> t;
+	while (t--) {
+		std::cin >> n;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				std::cout << (j == 1 || j == i) << ' ';
+			}
+
+			std::cout << '\n';
+		}
+	}
+
+	return 0;
+}
