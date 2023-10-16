8fcaa914e9ee82f8dc2362a63e92338a88b46d48
Denis <96687186+RexarX@users.noreply.github.com>
2023-10-16 10:38:37 +0300

    Update 1873c.cpp

--- prj.codeforces/1873c.cpp
+++ prj.codeforces/1873c.cpp
@@ -5,22 +5,22 @@
 
 int main()
 {
-        int t;
-        char element;
-        std::cin >> t;
-        for (int i = 0; i < t; i++) {
-                int cnt(0);
-                for (int x = 1; x <= 10; x++) {
-                        for (int y = 1; y <= 10; y++) {
-                                std::cin >> element;
-                                if (element == 'X') {
-                                        cnt += std::min(std::min(x, 10 - x + 1), std::min(y, 10 - y + 1));
-                                }
-                        }
-                }
-
-                std::cout << cnt << '\n';
+  int t;
+  char element;
+  std::cin >> t;
+  for (int i = 0; i < t; i++) {
+    int cnt(0);
+    for (int x = 1; x <= 10; x++) {
+      for (int y = 1; y <= 10; y++) {
+        std::cin >> element;
+        if (element == 'X') {
+          cnt += std::min(std::min(x, 10 - x + 1), std::min(y, 10 - y + 1));
         }
+      }
+    }
 
-        return 0;
+    std::cout << cnt << '\n';
+  }
+
+  return 0;
 }
