fdf4a781a63e80002d8cdf187529e2f3fd805787
Denis <96687186+RexarX@users.noreply.github.com>
2023-10-21 22:38:14 +0300

    Update 1512b.cpp

--- prj.codeforces/1512b.cpp
+++ prj.codeforces/1512b.cpp
@@ -4,15 +4,9 @@
 #include <vector>
 
 void alg(std::vector<std::string>& v, const int& n) {
-  struct Position {
-    int x;
-    int y;
-  };
-  Position pos1, pos2;
-  pos1.x = NULL;
-  pos1.y = NULL;
-  pos2.x = NULL;
-  pos2.y = NULL;
+  std::pair<int, int> pos1, pos2;
+  pos1 = std::make_pair(-1, -1);
+  pos2 = std::make_pair(-1, -1);
   for (int i = 0; i < n; i++) {
     if (std::count(v[i].begin(), v[i].end(), '*') == 2) {
       if (i == n - 1) {
@@ -26,30 +20,30 @@ void alg(std::vector<std::string>& v, const int& n) {
     }
     int pos = v[i].find('*');
     if (pos != std::string::npos) {
-      if (pos1.x == NULL && pos1.y == NULL) {
-        pos1.x = i;
-        pos1.y = pos;
+      if (pos1.first == -1 && pos1.second == -1) {
+        pos1.first = i;
+        pos1.second = pos;
       }
       else {
-        pos2.x = i;
-        pos2.y = pos;
+        pos2.first = i;
+        pos2.second = pos;
         break;
       }
     }
   }
-  if (pos1.y == pos2.y) {
-    if (pos1.y == (n - 1) / 2 && n > 2) {
-      v[pos1.x][0] = '*';
-      v[pos2.x][0] = '*';
+  if (pos1.second == pos2.second) {
+    if (pos1.second == (n - 1) / 2 && n > 2) {
+      v[pos1.first][0] = '*';
+      v[pos2.first][0] = '*';
     }
     else {
-      v[pos1.x][std::abs(n - 1 - pos2.y)] = '*';
-      v[pos2.x][std::abs(n - 1 - pos1.y)] = '*';
+      v[pos1.first][std::abs(n - 1 - pos2.second)] = '*';
+      v[pos2.first][std::abs(n - 1 - pos1.second)] = '*';
     }
   }
   else {
-    v[pos1.x][pos2.y] = '*';
-    v[pos2.x][pos1.y] = '*';
+    v[pos1.first][pos2.second] = '*';
+    v[pos2.first][pos1.second] = '*';
   }
 }
 
