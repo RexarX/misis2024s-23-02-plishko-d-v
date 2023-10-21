7bf6d0062ea7992807c52e645b9077f24b0674f2
Denis <96687186+RexarX@users.noreply.github.com>
2023-10-21 22:09:15 +0300

    Update 1512b.cpp

--- prj.codeforces/1512b.cpp
+++ prj.codeforces/1512b.cpp
@@ -4,15 +4,52 @@
 #include <vector>
 
 void alg(std::vector<std::string>& v, const int& n) {
+  struct Position {
+    int x;
+    int y;
+  };
+  Position pos1, pos2;
+  pos1.x = NULL;
+  pos1.y = NULL;
+  pos2.x = NULL;
+  pos2.y = NULL;
   for (int i = 0; i < n; i++) {
     if (std::count(v[i].begin(), v[i].end(), '*') == 2) {
-      v[n - 1] = v[i];
-      break;
+      if (i == n - 1) {
+        v[0] = v[i];
+        return;
+      }
+      else {
+        v[n - 1] = v[i];
+        return;
+      }
     }
     int pos = v[i].find('*');
     if (pos != std::string::npos) {
-      // v[i] = 
+      if (pos1.x == NULL && pos1.y == NULL) {
+        pos1.x = i;
+        pos1.y = pos;
+      }
+      else {
+        pos2.x = i;
+        pos2.y = pos;
+        break;
+      }
     }
+  }
+  if (pos1.y == pos2.y) {
+    if (pos1.y == (n - 1) / 2 && n > 2) {
+      v[pos1.x][0] = '*';
+      v[pos2.x][0] = '*';
+    }
+    else {
+      v[pos1.x][std::abs(n - 1 - pos2.y)] = '*';
+      v[pos2.x][std::abs(n - 1 - pos1.y)] = '*';
+    }
+  }
+  else {
+    v[pos1.x][pos2.y] = '*';
+    v[pos2.x][pos1.y] = '*';
   }
 }
 
