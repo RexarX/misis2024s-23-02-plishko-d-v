6990dff3917d967aacb5e9e964905a9735bbf57b
Denis <96687186+RexarX@users.noreply.github.com>
2023-10-21 20:06:09 +0300

    Update 0339b.cpp

--- prj.codeforces/0339b.cpp
+++ prj.codeforces/0339b.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 
-
 int main()
 {
-	long long int n, m, a, previous(1), result(0);
+	const long long int n, m;
+	long long int a, previous(1), result(0);
 	std::cin >> n >> m;
 	for (int i = 0; i < m; i++) {
 		std::cin >> a;
 		result += (n + a - previous) % n;
 		previous = a;
 	}
-
 	std::cout << result;
 	return 0;
 }
