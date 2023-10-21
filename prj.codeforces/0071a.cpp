64a99aa1fccbe6c8dd28a11ce467fe8ea34f04f3
Denis <96687186+RexarX@users.noreply.github.com>
2023-10-21 19:57:51 +0300

    Update 0071a.cpp

--- prj.codeforces/0071a.cpp
+++ prj.codeforces/0071a.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <string>
 
-
 std::string alg(const std::string& word) {
 	int len = word.length();
 	if (len > 10) {
 		return word[0] + std::to_string(len - 2) + word[len - 1];
 	}
-
 	return word;
 }
 
@@ -20,6 +18,5 @@ int main()
 		std::cin >> word;
 		std::cout << alg(word) << '\n';
 	}
-
 	return 0;
 }
