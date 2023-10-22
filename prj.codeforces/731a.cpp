93b1648e7d7e902321b4fd73f52f1ee979decc42
Denis <rexarx@bk.ru>
2023-10-22 17:54:43 +0300

    added 731a.cpp

---
+++ prj.codeforces/731a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main() {
+	std::string name;
+	char current = 'a';
+	int rotation(0);
+	std::cin >> name;
+	for (int i = 0; i < name.length(); i++) {
+		rotation += std::min(std::abs(current - name[i]), 26 - std::abs(name[i] - current));
+		current = name[i];
+	}
+	std::cout << rotation;
+	return 0;
+}
