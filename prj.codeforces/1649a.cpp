6c1c7813a3f318e68f9ea75830730ad3f6db2be8
Denis <rexarx@bk.ru>
2023-10-22 15:41:45 +0300

    added 1649a.cpp

---
+++ prj.codeforces/1649a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+
+int main() {
+	int t, n, right, left, a;
+	std::array<bool, 100> mas{};
+	std::cin >> t;
+	for (int i = 0; i < t; i++) {
+		std::cin >> n;
+		right = n;
+		left = 1;
+		for (int j = 1; j <= n; j++) {
+			std::cin >> mas[j];
+		}
+		while (mas[left]) {
+			left++;
+		}
+		while (mas[right]) {
+			right--;
+		}
+		std::cout << std::max(right - left + 2, 0) << '\n';
+	}
+	return 0;
+}
