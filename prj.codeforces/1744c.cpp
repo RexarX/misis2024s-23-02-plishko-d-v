70a30f53ae3b69498304eb1627b5df81bffb37df
Denis <96687186+RexarX@users.noreply.github.com>
2023-10-23 17:20:10 +0300

    Update 1744c.cpp

--- prj.codeforces/1744c.cpp
+++ prj.codeforces/1744c.cpp
@@ -3,18 +3,18 @@
 #include <algorithm>
 
 int alg(std::string& s, const int& n, const int& c) {
-    int result;
-    return result;
+  int result;
+  return result;
 }
 
 int main() {
-    int t, n, c;
-    std::string s;
-    std::cin >> t;
-    for (int i = 0; i < t; i++) {
-        std::cin >> n >> c;
-        std::cin >> s;
-        std::cout << alg(symbols, n, c) << '\n';
-    }
-    return 0;
+  int t, n, c;
+  std::string s;
+  std::cin >> t;
+  for (int i = 0; i < t; i++) {
+      std::cin >> n >> c;
+      std::cin >> s;
+      std::cout << alg(symbols, n, c) << '\n';
+  }
+  return 0;
 }
