ab308c10e2ad9b9aeac88fa1d4d98ff2ac6a362d
student <student@DESKTOP-K8MJKUR>
2023-10-23 12:08:25 +0300

    added 1690d.cpp

--- prj.codeforces/1690d.cpp
+++ prj.codeforces/1690d.cpp
@@ -2,17 +2,35 @@
 #include <string>
 #include <algorithm>
 
-int alg(std::string& s, const int& n, const int& c) {
-    
+int alg(std::string& symbols, const int& n, const int& k) {
+    int result, cnt(0);
+    for (int i = 0; i < k; i++) {
+        if (symbols[i] == 'W') {
+            cnt++;
+        }
+    }
+    result = cnt;
+    for (int i = k; i < n; i++) {
+        if (symbols[i] == 'W') {
+            cnt++;
+        }
+        if (symbols[i - k] == 'W') {
+            cnt--;
+        }
+        result = std::min(result, cnt);
+    }
+    return result;
 }
 
 int main() {
-    int t, n, c;
-    std::string s;
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t, k, n;
+    std::string symbols;
     std::cin >> t;
     for (int i = 0; i < t; i++) {
-        std::cin >> n >> c;
-        std::cin >> s;
+        std::cin >> n >> k;
+        std::cin >> symbols;
         std::cout << alg(symbols, n, k) << '\n';
     }
     return 0;
