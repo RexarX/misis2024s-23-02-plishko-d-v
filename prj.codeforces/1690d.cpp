b68df8e7ed3ba93d7f8e9390f165ed7e7b48299d
student <student@DESKTOP-K8MJKUR>
2023-10-23 12:06:43 +0300

    added 1690d.cpp

---
+++ prj.codeforces/1690d.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int alg(std::string& s, const int& n, const int& c) {
+    
+}
+
+int main() {
+    int t, n, c;
+    std::string s;
+    std::cin >> t;
+    for (int i = 0; i < t; i++) {
+        std::cin >> n >> c;
+        std::cin >> s;
+        std::cout << alg(symbols, n, k) << '\n';
+    }
+    return 0;
+}
