e039fdf7eb2c070380898dc771254e6217665296
Denis <rexarx@bk.ru>
2023-10-28 18:07:55 +0300

    Revert "Update 1330a.cpp"
    This reverts commit 09fdf1709ab7bab759c5c4ab0c064ae640b46bb5.

--- prj.codeforces/1330a.cpp
+++ prj.codeforces/1330a.cpp
@@ -10,7 +10,7 @@ int main()
 		std::vector<bool> check(201);
 		for (int j = 0; j < n; j++) {
 			std::cin >> element;
-			check.at(--element) = true;
+			check[--element] = true;
 		}
 		int k(0), cnt(0);
 		while (cnt < x || check.at(k)) {
