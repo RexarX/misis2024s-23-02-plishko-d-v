46b5dfaa150b8adece43dbde895a9e1f56a2ff04
Denis <96687186+RexarX@users.noreply.github.com>
2023-10-31 11:35:44 +0300

    Update 1872a.cpp

--- prj.codeforces/1872a.cpp
+++ prj.codeforces/1872a.cpp
@@ -3,12 +3,12 @@
 
 int main()
 {
-    std::ios::sync_with_stdio(false);
-    int t, a, b, c;
-	std::cin >> t;
-	for (int i = 0; i < t; ++i) {
-		std::cin >> a >> b >> c;
-		std::cout << (std::max(a, b) - (a + b) / 2 + c - 1) / c << '\n';
-	}
-	return 0;
+  std::ios::sync_with_stdio(false);
+  int t, a, b, c;
+  std::cin >> t;
+  for (int i = 0; i < t; ++i) {
+    std::cin >> a >> b >> c;
+	std::cout << (std::max(a, b) - (a + b) / 2 + c - 1) / c << '\n';
+  }
+  return 0;
 }
