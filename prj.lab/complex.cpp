692d2f259e3e461a17a96248304466ef69412dfa
student <student@DESKTOP-K8MJKUR>
2023-11-06 12:33:45 +0300

    upd: complex.cpp

--- prj.lab/complex.cpp
+++ prj.lab/complex.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <cmath>
 
 struct Complex {
   Complex() = default; //default constructor
@@ -9,10 +10,12 @@ struct Complex {
   bool operator!=(const Complex& rhs) const { return !operator==(rhs); } //inequality comparison operator that checks if two complex numbers are not equal
   Complex& operator+=(const Complex& rhs); //addition assignment operator that adds another complex number to the current complex number
   Complex& operator+=(const double rhs) { return operator+=(Complex(rhs)); } //addition assignment operator that adds a real number to the current complex number
-  // Complex& operator-=(const Complex& rhs);
-  // Complex& operator-=(const double rhs) { return operator-=(Complex(rhs)); }
-  // Complex& operator*=(const Complex& rhs);
-  Complex& operator*=(const double rhs); //multiplication assignment operator that multiplies the current complex number by a real number
+  Complex& operator-=(const Complex& rhs); //subtraction assignment that subtracts another complex number and the current complex number
+  Complex& operator-=(const double rhs) { return operator-=(Complex(rhs)); } //subtraction assignment that subtracts a real number and the current complex number
+  Complex& operator*=(const Complex& rhs); //multiplication assignment operator that multiplies another complex number to the current complex number
+  Complex& operator*=(const double rhs) { return operator*=(Complex(rhs)); } //multiplication assignment operator that multiplies the current complex number by a real number
+  Complex& operator/=(const Complex& rhs); //division assignment operator that multiplies another complex number to the current complex number
+  Complex& operator/=(const double rhs) { return operator/=(Complex(rhs)); } //division assignment operator that multiplies the current complex number by a real number
   std::ostream& writeTo(std::ostream& ostrm) const; //function that writes the complex number to an output stream
   std::istream& readFrom(std::istream& istrm); //function that reads a complex number from an input stream
 
@@ -26,6 +29,7 @@ struct Complex {
 
 Complex operator+(const Complex& lhs, const Complex& rhs); //non-member function, addition operator that adds two complex numbers and returns the result
 Complex operator-(const Complex& lhs, const Complex& rhs); //non-member function, subtraction operator that subtracts one complex number from another and returns the result 
+Complex operator*(const Complex& lhs, const Complex& rhs);
 
 inline std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs) {
   return rhs.writeTo(ostrm);
@@ -51,6 +55,17 @@ bool testParse(const std::string& str) {
 int main() {
   Complex z;
   z += Complex(8.0);
+  z += 4.0;
+  z = Complex(8.0) + Complex(4.0);
+  z -= Complex(8.0);
+  z -= 4.0;
+  z = Complex(8.0) - Complex(4.0);
+  z *= Complex(8.0);
+  z *= 4.0;
+  z = Complex(8.0) * Complex(4.0);
+  /*z /= Complex(8.0);
+  z /= 4.0;
+  z = Complex(8.0) / Complex(4.0); */
   testParse("{8.9,9}");
   testParse("{8.9, 9}");
   testParse("{8.9,9");
@@ -73,6 +88,12 @@ Complex& Complex::operator+=(const Complex& rhs) {
   return *this;
 }
 
+Complex& Complex::operator-=(const Complex& rhs) {
+  re -= rhs.re;
+  im -= rhs.im;
+  return *this;
+}
+
 Complex operator+(const Complex& lhs, const Complex& rhs) {
   Complex sum(lhs);
   sum += rhs;
@@ -83,10 +104,42 @@ Complex operator-(const Complex& lhs, const Complex& rhs) {
   return Complex(lhs.re - rhs.re, lhs.im - rhs.im);
 }
 
-Complex& Complex::operator*=(const double rhs) {
+Complex& Complex::operator*=(const Complex& rhs) {
+  re = re * rhs.re - im * rhs.im;
+  im = re * rhs.im + im * rhs.re;
+  return *this;
+}
+
+/*Complex& Complex::operator*=(const double rhs) {
   re *= rhs;
   im *= rhs;
   return *this;
+} */
+
+Complex operator*(const Complex& lhs, const Complex& rhs) {
+  Complex multiply;
+  multiply.re = lhs.re * rhs.re - lhs.im * rhs.im;
+  multiply.im = lhs.re * rhs.im + lhs.im * rhs.re;
+  return multiply;
+}
+
+Complex& Complex::operator/=(const Complex& rhs) {
+  re = (re * rhs.re + im * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im);
+  im = (rhs.re * im - re * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im);
+  return *this;
+}
+
+/*Complex& Complex::operator/=(const double rhs) {
+  re = re / rhs;
+  im = im / rhs;
+  return *this;
+} */
+
+Complex operator/(const Complex& lhs, const Complex& rhs) {
+  Complex division;
+  division.re = (lhs.re * rhs.re + lhs.im * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im);
+  division.im = (rhs.re * lhs.im - lhs.re * rhs.im) / (rhs.re * rhs.re + rhs.im * rhs.im);
+  return division;
 }
 
 std::ostream& Complex::writeTo(std::ostream& ostrm) const {
