f71e61971e04c74b9bc61651f6cc05ae5f512c57
Vergil <Vergil@DESKTOP-NESDP5L>
2023-12-01 12:50:57 +0300

    UPD: test_rational.cpp

--- prj.test/test_rational.cpp
+++ prj.test/test_rational.cpp
@@ -7,7 +7,7 @@ TEST_CASE("rational ctor") {
   Rational r_def;
   CHECK(0 == r_def.num());
   CHECK(1 == r_def.den());
-  
+
   Rational r_int(3);
   CHECK(3 == r_int.num());
   CHECK(1 == r_int.den());
@@ -16,6 +16,11 @@ TEST_CASE("rational ctor") {
   CHECK(-2 == q.num());
   CHECK(3 == q.den());
 
+  CHECK_THROWS(Rational(1, 0));
+}
+
+TEST_CASE("rational bool operator") {
+  Rational q(2, -3);
   CHECK(1 == (q == Rational(-2, 3)));
   CHECK(1 == (q != Rational(2)));
 
@@ -26,6 +31,10 @@ TEST_CASE("rational ctor") {
 
   CHECK(Rational(1, 2) > q);
   CHECK(Rational(-3, 4) < q);
+}
+
+TEST_CASE("rational arithmetic operator") {
+  Rational q(2, -3);
 
   q += Rational(-2, 3);
   CHECK(Rational(-4, 3) == q);
@@ -42,6 +51,4 @@ TEST_CASE("rational ctor") {
   q /= Rational(-2, 3);
   CHECK(Rational(6, 18) == q);
   CHECK(Rational(18, 18) == (q / Rational(1, 3)));
-
-  CHECK_THROWS(Rational(1, 0));
 }
