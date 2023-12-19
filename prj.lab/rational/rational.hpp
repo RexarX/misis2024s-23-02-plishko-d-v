b8dd7403fa098ad6de859e4918d5d2b5700ca754
Denis <96687186+RexarX@users.noreply.github.com>
2023-12-19 20:40:07 +0300

    Update rational.hpp

--- prj.lab/rational/rational.hpp
+++ prj.lab/rational/rational.hpp
@@ -4,12 +4,13 @@
 class Rational {
 public:
   Rational() = default;
-  explicit Rational(const int64_t num);
-  Rational(const int64_t num, const int64_t den);
+  explicit Rational(const std::int64_t num);
+  Rational(const std::int64_t num, const std::int64_t den);
   ~Rational() = default;
+  Rational& operator=(const Rational&) = default;
 
-  int64_t num() const { return num_; }
-  int64_t den() const { return den_; }
+  std::int64_t num() const { return num_; }
+  std::int64_t den() const { return den_; }
 
   bool operator==(const Rational& lhs) const { return num_ * lhs.den_ == lhs.num_ * den_; }
   bool operator!=(const Rational& lhs) const { return !operator==(lhs); }
@@ -19,20 +20,20 @@ public:
   bool operator<=(const Rational& lhs) const { return operator==(lhs) || operator<(lhs); }
 
   Rational& operator+=(const Rational& lhs);
-  Rational& operator+=(const int64_t& lhs) { return operator+=(Rational(lhs)); }
+  Rational& operator+=(const std::int64_t& lhs) { return operator+=(Rational(lhs)); }
   Rational& operator-=(const Rational& lhs);
-  Rational& operator-=(const int64_t& lhs) { return operator-=(Rational(lhs)); }
+  Rational& operator-=(const std::int64_t& lhs) { return operator-=(Rational(lhs)); }
   Rational& operator*=(const Rational& lhs);
-  Rational& operator*=(const int64_t& lhs) { return operator*=(Rational(lhs)); }
+  Rational& operator*=(const std::int64_t& lhs) { return operator*=(Rational(lhs)); }
   Rational& operator/=(const Rational& lhs);
-  Rational& operator/=(const int64_t& lhs) { return operator/=(Rational(lhs)); }
+  Rational& operator/=(const std::int64_t& lhs) { return operator/=(Rational(lhs)); }
 
   std::ostream& writeTo(std::ostream& ostrm) const;
   std::istream& readFrom(std::istream& istrm);
 
 private:
-  int64_t num_ = 0;
-  int64_t den_ = 1;
+  std::int64_t num_ = 0;
+  std::int64_t den_ = 1;
 };
 
 inline Rational operator+(const Rational& lhs, const Rational& rhs);
@@ -40,12 +41,12 @@ inline Rational operator-(const Rational& lhs, const Rational& rhs);
 inline Rational operator*(const Rational& lhs, const Rational& rhs);
 inline Rational operator/(const Rational& lhs, const Rational& rhs);
 
-inline Rational operator+(const Rational& lhs, const int64_t& rhs) { return operator+(lhs, Rational(rhs)); }
-inline Rational operator-(const Rational& lhs, const int64_t& rhs) { return operator-(lhs, Rational(rhs)); }
-inline Rational operator*(const Rational& lhs, const int64_t& rhs) { return operator*(lhs, Rational(rhs)); }
-inline Rational operator/(const Rational& lhs, const int64_t& rhs) { return operator/(lhs, Rational(rhs)); }
+inline Rational operator+(const Rational& lhs, const std::int64_t& rhs) { return operator+(lhs, Rational(rhs)); }
+inline Rational operator-(const Rational& lhs, const std::int64_t& rhs) { return operator-(lhs, Rational(rhs)); }
+inline Rational operator*(const Rational& lhs, const std::int64_t& rhs) { return operator*(lhs, Rational(rhs)); }
+inline Rational operator/(const Rational& lhs, const std::int64_t& rhs) { return operator/(lhs, Rational(rhs)); }
 
-inline Rational operator+(const int64_t& lhs, const Rational& rhs) { return operator+(Rational(lhs), rhs); }
-inline Rational operator-(const int64_t& lhs, const Rational& rhs) { return operator-(Rational(lhs), rhs); }
-inline Rational operator*(const int64_t& lhs, const Rational& rhs) { return operator*(Rational(lhs), rhs); }
-inline Rational operator/(const int64_t& lhs, const Rational& rhs) { return operator/(Rational(lhs), rhs); }
+inline Rational operator+(const std::int64_t& lhs, const Rational& rhs) { return operator+(Rational(lhs), rhs); }
+inline Rational operator-(const std::int64_t& lhs, const Rational& rhs) { return operator-(Rational(lhs), rhs); }
+inline Rational operator*(const std::int64_t& lhs, const Rational& rhs) { return operator*(Rational(lhs), rhs); }
+inline Rational operator/(const std::int64_t& lhs, const Rational& rhs) { return operator/(Rational(lhs), rhs); }
